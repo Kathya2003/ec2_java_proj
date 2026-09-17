fde0548168ed09871a77a9d04a4eb146cdde772e
Kathya2003 <rockingkathyayini1647@gmail.com>
2026-09-17 19:12:40 +0530

    Update Program1.java

--- Program1.java
+++ Program1.java
@@ -1,6 +1,6 @@
 class Program1{
  public static void main(String []args){
   System.out.println("java project created in ec2 instance");
-  System.out.println("webhook and email configurations are done");
+  System.out.println("webhook and email configurations are  done");
  }
 }
